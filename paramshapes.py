16238b67d62c1792bbe65500cec2545ec7db82cd
Andres Martinez Colan <a.i.martinezcolan@student.tudelft.nl>
2022-01-17 18:46:30 +0000

    Update paramshapes.py Creation of 2-point numerical integration for Line2 Creation of Line3 element for quadratic interpolation

--- paramshapes.py
+++ paramshapes.py
@@ -68,6 +68,17 @@ class Line2Shape(Shape):
 
             self._ips[0, 0] = 0
             self._wts[0] = 2
+
+        elif self._int == 'Gauss2':
+            self._ipcount = 2
+            self._ips = np.zeros((1, 2))
+            self._wts = np.zeros(2)
+
+            self._ips[0, 0] = -1 / np.sqrt(3)
+            self._ips[0, 1] = 1 / np.sqrt(3)
+            self._wts[0] = 1
+            self._wts[1] = 1
+
         else:
             raise SyntaxError('Unsupported integration scheme for Line2 shape')
 
@@ -95,6 +106,57 @@ class Line2Shape(Shape):
         return self._N
 
 
+class Line3Shape(Shape):
+    def __init__(self, intscheme):
+        print('Creating Line3Shape...')
+
+        self._ncount = 3
+        self._rank = 1
+        self._int = intscheme
+
+        if self._int == 'Gauss2':
+            self._ipcount = 2
+            self._ips = np.zeros((1, 2))
+            self._wts = np.zeros(2)
+
+            self._ips[0, 0] = -1 / np.sqrt(3)
+            self._ips[0, 1] = 1 / np.sqrt(3)
+            self._wts[0] = 1
+            self._wts[1] = 1
+
+        else:
+            raise SyntaxError('Unsupported integration scheme for Line3 shape')
+
+        self._N = np.zeros((3, 1, self._ipcount))
+        self._dN = np.zeros((3, 1, self._ipcount))
+
+        self._N[0, :, :] = 0.5 * self._ips[0, :] * (self._ips[0, :] - 1)
+        self._N[1, :, :] = 1 - self._ips[0, :] ** 2
+        self._N[2, :, :] = 0.5 * self._ips[0, :] * (self._ips[0, :] + 1)
+
+        self._dN[0, 0, :] = self._ips[0, :] - 0.5
+        self._dN[1, 0, :] = -2 * self._ips[0, :]
+        self._dN[2, 0, :] = self._ips[0, :] + 0.5
+
+    def get_shape_gradients(self, coords):
+        wts = self._wts
+        dN = self._dN
+
+        for ip in range(self._ipcount):
+            print(np.shape(coords))
+            print(coords)
+            print(np.shape(dN[:, :, ip]))
+            J = np.matmul(coords, dN[:, :, ip]).item()
+            wts[ip] *= J
+            dN[:, :, ip] = dN[:, :, ip] / J
+
+        return dN, wts
+
+    def get_shape_functions(self):
+        return self._N
+
+
 def declare(factory):
     factory.declare_shape('Triangle3', Tri3Shape)
     factory.declare_shape('Line2', Line2Shape)
+    factory.declare_shape('Line3', Line3Shape)

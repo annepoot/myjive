c0b57f49acbb6b7de21aebec6d1044a034a52e1b
Anne Poot <a.poot-1@tudelft.nl>
2022-05-18 15:53:08 +0200

    created tapered beam test

--- barmodel.py
+++ barmodel.py
@@ -4,10 +4,12 @@ from names import Actions as act
 from names import ParamNames as pn
 from names import GlobNames as gn
 from names import PropNames as prn
-from model import *
+from model import Model
 
 ELEMENTS = 'elements'
 EA = 'EA'
+E = 'E'
+A = 'A'
 k = 'k'
 q = 'q'
 rho = 'rho'
@@ -28,7 +30,23 @@ class BarModel(Model):
             self._get_body_force(params, globdat)
 
     def configure(self, props, globdat):
-        self._EA = float(props[EA])
+        if EA in props:
+            self._E = props[EA]
+            self._A = 1.0
+        else:
+            self._E = props[E]
+            self._A = props[A]
+
+        try:
+            self._A = float(self._A)
+        except:
+            pass
+
+        try:
+            self._E = float(self._E)
+        except:
+            pass
+
         self._k = float(props.get(k, 0))
         self._q = float(props.get(q, 0))
         self._rho = float(props.get(rho,1))
@@ -49,8 +67,10 @@ class BarModel(Model):
                 globdat[gn.DOFSPACE].add_dof(node, doftype)
 
     def _get_matrix(self, params, globdat):
-        D = np.array([[self._EA]])
-        K = np.array([[self._k]])
+        const_E = type(self._E) is float
+        const_A = type(self._A) is float
+        const_k = type(self._k) is float
+
         for elem in self._elems:
             inodes = elem.get_nodes()
             idofs = globdat[gn.DOFSPACE].get_dofs(inodes, DOFTYPES[0:self._rank])
@@ -64,13 +84,29 @@ class BarModel(Model):
                 N = np.zeros((1, self._nodecount))
                 B = grads[:, :, ip].transpose()
                 N[0, :] = sfuncs[:, ip].transpose()
+
+                if not const_E or not const_A or not const_k:
+                    x = self._shape.get_global_point(self._shape.get_integration_points()[:,ip], coords)[0]
+
+                E_ = self._E if const_E else eval(self._E)
+                A_ = self._A if const_A else eval(self._A)
+                k_ = self._k if const_k else eval(self._k)
+
+                D = np.array([[E_ * A_]])
+                K = np.array([[k_]])
+
                 elmat += weights[ip] * (
                             np.matmul(np.transpose(B), np.matmul(D, B)) + np.matmul(np.transpose(N), np.matmul(K, N)))
 
             params[pn.MATRIX0][np.ix_(idofs, idofs)] += elmat
 
     def _get_mass_matrix(self, params, globdat):
-        M = np.array([[self._rho]])
+        const_rho = type(self._rho) is float
+        const_A = type(self._A) is float
+
+        if const_rho and const_A:
+            M = np.array([[self._rho * self._A]])
+
         for elem in self._elems:
             inodes = elem.get_nodes()
             idofs = globdat[gn.DOFSPACE].get_dofs(inodes, DOFTYPES[0:self._rank])
@@ -82,6 +118,15 @@ class BarModel(Model):
             for ip in range(self._ipcount):
                 N = np.zeros((1, self._nodecount))
                 N[0, :] = sfuncs[:, ip].transpose()
+
+                if not const_rho or not const_A:
+                    x = self._shape.get_global_point(self._shape.get_integration_points()[:,ip], coords)[0]
+
+                rho_ = self._rho if const_rho else eval(self._rho)
+                A_ = self._A if const_A else eval(self._A)
+
+                M = np.array([[rho_ * A_]])
+
                 elmat += weights[ip] * np.matmul(np.transpose(N), np.matmul(M, N))
 
             params[pn.MATRIX2][np.ix_(idofs, idofs)] += elmat

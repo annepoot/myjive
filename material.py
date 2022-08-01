a9be9c8dc805e260d63470af4c111419bfc3cd93
Anne Poot <a.poot-1@tudelft.nl>
2022-08-01 13:37:53 +0200

    created Material base class

---
+++ material.py
@@ -0,0 +1,14 @@
+
+class Material:
+
+    def __init__(self, name, props, conf, rank):
+        pass
+
+    def configure(self, props, globdat):
+        pass
+
+    def get_config(self, globdat):
+        pass
+    
+    def create_int_points(self, npoints):
+        pass

7201491bdd5db542ba699f118dd975b21f832404
Anne Poot <a.poot-1@tudelft.nl>
2022-08-01 18:57:53 +0200

    created basic isotropic and heterogeneous materials

---
+++ heterogeneousmaterial.py
@@ -0,0 +1,39 @@
+from isotropicmaterial import IsotropicMaterial
+from isotropicmaterial import E_PROP, NU_PROP, AREA_PROP, THICKNESS_PROP, ANMODEL_PROP
+import proputils as pu
+
+
+class HeterogeneousMaterial(IsotropicMaterial):
+
+    def configure(self, props):
+
+        self._anmodel = props.get(ANMODEL_PROP, self._anmodel).upper()
+        assert self._is_valid_anmodel(self._anmodel), 'Analysis model ' + self._anmodel + ' not valid for rank ' + str(self._rank)
+
+        self._E = props.get(E_PROP, self._E)
+        self._nu = props.get(NU_PROP, self._nu)
+
+        self._E = pu.soft_cast(self._E, float)
+        self._nu = pu.soft_cast(self._nu, float)
+
+        if self._rank == 1:
+            self._area = props.get(AREA_PROP, self._area)
+            self._area = pu.soft_cast(self._area, float)
+        elif self._rank == 2:
+            self._thickness = props.get(THICKNESS_PROP, self._thickness)
+            self._thickness = pu.soft_cast(self._thickness, float)
+
+    def stiff_at_point(self, ipoint=None):
+        return self._compute_stiff_matrix(ipoint)
+
+    def _get_E(self, ipoint=None):
+        return pu.evaluate(self._E, ipoint, self._rank)
+
+    def _get_nu(self, ipoint=None):
+        return pu.evaluate(self._nu, ipoint, self._rank)
+
+    def _get_area(self, ipoint=None):
+        return pu.evaluate(self._area, ipoint, self._rank)
+
+    def _get_thickness(self, ipoint=None):
+        return pu.evaluate(self._thickness, ipoint, self._rank)
--- material.py
+++ material.py
@@ -1,14 +1,35 @@
+from isotropicmaterial import IsotropicMaterial
+from heterogeneousmaterial import HeterogeneousMaterial
+
+MATERIAL = 'material'
+TYPE = 'type'
+RANK = 'rank'
+
+
+def new_material(name, props):
+    myprops = props.get(MATERIAL)
+    typ = myprops[TYPE]
+    rank = myprops[RANK]
+
+    if typ == 'Isotropic':
+        mat = IsotropicMaterial(rank)
+    elif typ == 'Heterogeneous':
+        mat = HeterogeneousMaterial(rank)
+    else:
+        raise ValueError(typ + ' is not a valid material')
+
+    mat.configure(props)
+
+    return mat
+
 
 class Material:
 
-    def __init__(self, name, props, conf, rank):
+    def __init__(self, rank):
         pass
 
-    def configure(self, props, globdat):
+    def configure(self, props):
         pass
 
-    def get_config(self, globdat):
-        pass
-    
-    def create_int_points(self, npoints):
+    def get_config(self):
         pass

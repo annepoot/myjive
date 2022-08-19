d98e1d95c7fc614cc14ba7e11d1a4eae4cd6f5cb
Anne Poot <a.poot-1@tudelft.nl>
2022-08-19 18:18:00 +0200

    created separate image script for 1d bar problem

---
+++ gptapered/prior-posterior.pro
@@ -0,0 +1,114 @@
+gpinit =
+{
+  type = GPInit;
+
+  mesh =
+  {
+    type = manual;
+    file = 2nodebar.mesh;
+  };
+
+  coarseMesh =
+  {
+    type = manual;
+    file = 2nodebar_coarse.mesh;
+  };
+
+  nodeGroups = [ left, right, mid ];
+
+  left =
+  {
+    xtype = min;
+  };
+
+  right =
+  {
+    xtype = max;
+  };
+
+  mid =
+  {
+    xtype = mid;
+  };
+};
+
+gpsolver =
+{
+  type = GPSolver;
+
+  nsteps = 1;
+  storeMatrix = True;
+  storeConstraints = True;
+  getUnitMassMatrix = True;
+  getForceResults = True;
+};
+
+gpsampler =
+{
+  type = GPSampler;
+
+  nsample = 30;
+  seed = 0;
+};
+
+model =
+{
+  type = Multi;
+
+  models = [ solid, gp, diri ];
+
+  solid =
+  {
+    type = Solid;
+
+    elements = all;
+
+    material =
+    {
+      type = Heterogeneous;
+      rank = 1;
+      anmodel = bar;
+
+      E = 3.0 - 0.29 * x;
+      rho = 0.1;
+    };
+
+    shape =
+    {
+      type = Line2;
+      intScheme = Gauss2;
+    };
+  };
+
+  gp =
+  {
+    type = GPf;
+
+    prior =
+    {
+      type = SPDE;
+      func = alpha**2 * M;
+      hyperparams =
+      {
+        alpha = 0.1;
+      };
+    };
+
+    obsNoise = 1e-5;
+
+    shape =
+    {
+      type = Line2;
+      intScheme = Gauss2;
+    };
+  };
+
+  diri =
+  {
+    type = Dirichlet;
+
+    groups = [ left, right ];
+    dofs   = [ dx, dx ];
+    values = [ 0.0, 1.0 ];
+  };
+};
---
+++ gptapered/prior-posterior.py
@@ -0,0 +1,91 @@
+import sys
+sys.path.append('../')
+
+from math import exp
+import matplotlib.pyplot as plt
+import numpy as np
+import main
+import proputils as pu
+import testutils as tu
+from copy import deepcopy
+
+def mesher_lin(L, n, fname='2nodebar_coarse'):
+    dx = L / n
+    if not '.' in fname:
+        fname += '.mesh'
+
+    with open(fname, 'w') as fmesh:
+        fmesh.write('nodes (ID, x, [y], [z])\n')
+        for i in range(n + 1):
+            fmesh.write('%d %f\n' % (i, i * dx))
+        fmesh.write('elements (node#1, node#2, [node#3, ...])\n')
+        for i in range(n):
+            fmesh.write('%d %d\n' % (i, i + 1))
+
+props = pu.parse_file('tapered.pro')
+
+props_c = {}
+props_c['gpinit'] = deepcopy(props['gpinit'])
+props_c['gpsolver'] = deepcopy(props['gpsolver'])
+props_c['model'] = deepcopy(props['model'])
+props_c['gpinit']['mesh']['file'] = '2nodebar_coarse.mesh'
+
+globdat_c = main.jive(props_c)
+u_coarse = globdat_c['state0']
+
+globdat = main.jive(props)
+K = globdat['matrix0']
+M = globdat['matrix2']
+u = globdat['state0']
+f = globdat['extForce']
+c = globdat['constraints']
+
+Kc, fc = c.constrain(K, f)
+
+xf = np.linspace(0, 10, len(u))
+xc = np.linspace(0, 10, len(u_coarse))
+
+step = int((len(xf)-1) / (len(xc)-1))
+
+u_post = globdat['u_post']
+
+print(u_coarse)
+print(u[::step])
+print(u_post[::step])
+
+Phi = globdat['Phi']
+Phic = globdat['Phic']
+f_prior = globdat['f_prior']
+u_prior = globdat['u_prior']
+f_post = globdat['f_post']
+u_post = globdat['u_post']
+std_f_prior = np.sqrt(globdat['var_f_prior'])
+std_f_post = np.sqrt(globdat['var_f_post'])
+std_u_prior = np.sqrt(globdat['var_u_prior'])
+std_u_post = np.sqrt(globdat['var_u_post'])
+
+samples_u_prior = globdat['samples_u_prior']
+samples_f_prior = globdat['samples_f_prior']
+samples_u_post = globdat['samples_u_post']
+samples_f_post = globdat['samples_f_post']
+
+plt.figure(tight_layout=True)
+ax = plt.gca()
+ax.set_axis_off()
+plt.plot(xf, u_post, color='C0', label='posterior mean')
+plt.plot(xf, u_prior, color='C1', label='prior mean')
+plt.plot(xf, samples_u_post, color='C0', linewidth=0.3)
+plt.plot(xf, samples_u_prior, color='C1', linewidth=0.3)
+plt.fill_between(xf, u_post - 2*std_u_post, u_post + 2*std_u_post, color='C0', alpha=0.3)
+plt.fill_between(xf, u_prior - 2*std_u_prior, u_prior + 2*std_u_prior, color='C1', alpha=0.3)
+plt.plot(xc, u_coarse, color='C2', label='coarse solution')
+plt.plot(xf, u, color='C3', label='fine solution')
+plt.legend(fontsize=13)
+
+folder = '/home/anne/Storage/owncloud/phd/images/1d-bar/'
+
+plt.savefig(folder + 'prior-posterior.pdf', transparent=True)
+for dpi in [300, 600, 1200]:
+    plt.savefig(folder + 'prior-posterior-{}dpi.png'.format(dpi), dpi=dpi, transparent=True)
+
+plt.show()

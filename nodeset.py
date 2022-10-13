f87dd4f09f3c2dc5216846d57e89d087f5e73b25
Anne Poot <a.poot-1@tudelft.nl>
2022-10-13 11:45:41 +0200

    added find_nodes and get_node_ids functions

--- nodeset.py
+++ nodeset.py
@@ -14,8 +14,14 @@ class NodeSet(ItemSet):
     def find_node(self, node_id):
         return self.find_item(node_id)
 
+    def find_nodes(self, node_ids):
+        return self.find_items(node_ids)
+
     def get_node_id(self, inode):
         return self.get_item_id(inode)
+
+    def get_node_ids(self, inodes):
+        return self.get_item_ids(inodes)
 
     def get_node_coords(self, inode):
         return self._data[inode].get_coords()

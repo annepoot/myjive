41da440dc84d9146969f8560c7d39880874e0aa3
Anne Poot <a.poot-1@tudelft.nl>
2022-10-14 10:13:32 +0200

    cleaned up gpcantilever example

--- gpexamples/gpcantilever/superscript.py
+++ gpexamples/gpcantilever/superscript.py
@@ -18,7 +18,7 @@ props_c['init']['type'] = 'Init'
 props_c['solver'] = deepcopy(props['gpsolver'])
 props_c['solver']['type'] = 'Solver'
 props_c['model'] = deepcopy(props['model'])
-props_c['model']['models'] = '[ elastic, diri ]'
+props_c['model']['models'] = '[ solid, diri ]'
 props_c['init']['mesh']['file'] = 'beam_coarse.msh'
 
 globdat_c = main.jive(props_c)

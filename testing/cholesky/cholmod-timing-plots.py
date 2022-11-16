c2dafd1239367e47f48421f7c6d7537975acae81
Anne Poot <a.poot-1@tudelft.nl>
2022-11-16 09:56:34 +0100

    tested efficiency of simplicial and supernodal cholmod timings

---
+++ testing/cholesky/cholmod-timing-plots.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('runtimes.txt', delimiter=',', skip_header=1)
+
+N = data[:,0]
+simplicial_total = data[:,1]
+simplicial_analyze = data[:,2]
+simplicial_inplace = data[:,3]
+supernodal_total = data[:,4]
+supernodal_analyze = data[:,5]
+supernodal_inplace = data[:,6]
+
+O_simplical_fit = N**1.6 * 10**-8
+O_supernodal_fit = N**1 * 10**-4
+
+plt.figure()
+# plt.loglog(N, simplicial_total, label='simplicial total')
+plt.loglog(N, simplicial_analyze, label='simplicial analyze')
+plt.loglog(N, simplicial_inplace,label='simplicial inplace')
+# plt.loglog(N, supernodal_total, label='supernodal total')
+plt.loglog(N, supernodal_analyze, label='supernodal analyze')
+plt.loglog(N, supernodal_inplace, label='supernodal inplace')
+
+plt.loglog(N, O_simplical_fit, label='simplicial fit')
+plt.loglog(N, O_supernodal_fit, label='supernodal fit')
+plt.title
+plt.legend(bbox_to_anchor=(1, 1))
+plt.title('CHOLMOD runtimes')
+plt.xlabel(r'dofs')
+plt.ylabel(r'runtime')
+plt.show()

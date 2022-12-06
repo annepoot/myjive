62c73ec7eff4b3042e80977b14aa2a95485f4936
Anne Poot <a.poot-1@tudelft.nl>
2022-12-06 11:12:58 +0100

    Implemented numba-assisted sparse cholesky decomposition

--- jive/solver/numba/cholesky.py
+++ jive/solver/numba/cholesky.py
@@ -18,6 +18,16 @@ def incomplete_cholesky(A):
     return L
 
 
+def sparse_cholesky(A):
+    if not spsp.isspmatrix_csr(A):
+        raise ValueError('A has to be a sparse matrix in csr format')
+
+    Ldata, Lindices, Lindptr = sparse_cholesky_jit(A.data, A.indices, A.indptr)
+    L = spsp.csr_array((Ldata, Lindices, Lindptr), dtype=A.dtype)
+
+    return L
+
+
 ##########################
 # numba helper functions #
 ##########################
@@ -28,7 +38,7 @@ def incomplete_cholesky_jit(data, indices, indptr):
     rows, cols = idxs2rowscols(indices, indptr)
 
     # Get all row and column indices in pairs
-    for idx, row, col in zip(range(len(indices)), rows, cols):
+    for idx, (row, col) in enumerate(zip(rows, cols)):
         if data[idx] == 0.0:
             continue
 
@@ -66,10 +76,78 @@ def incomplete_cholesky_jit(data, indices, indptr):
         if row == col:
             L_ij = np.sqrt(A_ij - rowsum)
         else:
-            idx_jj = rowcol2idx(indices, indptr, col, col)
+            idx_jj = indptr[col+1]-1
             L_jj = L_data[idx_jj]
             L_ij = (A_ij - rowsum) / L_jj
 
         L_data[idx] = L_ij
 
     return L_data
+
+
+@njit
+def sparse_cholesky_jit(data, indices, indptr):
+    Ldata = []
+    Lindices = []
+    Lindptr = [0]
+
+    # Go over all rows, and all relevant columns
+    # (starting from the first non-zero column in that row)
+    for row in range(len(indptr)-1):
+        for col in range(indices[indptr[row]], row+1):
+
+            idx = rowcol2idx(indices, indptr, row, col)
+            if idx >= 0:
+                A_ij = data[idx]
+            else:
+                A_ij = 0
+
+            # Get all entries belonging to row i and row j
+            irowindices = Lindices[Lindptr[row]:]
+            irowvalues = Ldata[Lindptr[row]:]
+
+            if row == col:
+                jrowindices = irowindices
+                jrowvalues = irowvalues
+            else:
+                jrowindices = Lindices[Lindptr[col]:Lindptr[col+1]]
+                jrowvalues = Ldata[Lindptr[col]:Lindptr[col+1]]
+
+            # Initialize rowsum computation
+            rowsum = 0.0
+            iidx = 0
+            jidx = 0
+
+            # Compute sum(L_ik * Ljk) for 0 <= k < j
+            while iidx < len(irowindices) and jidx < len(jrowindices):
+                icol = irowindices[iidx]
+                jcol = jrowindices[jidx]
+
+                if icol >= col or jcol >= col:
+                    break
+
+                if icol < jcol:
+                    iidx += 1
+                elif icol > jcol:
+                    jidx += 1
+                else:
+                    rowsum += irowvalues[iidx] * jrowvalues[jidx]
+                    iidx += 1
+                    jidx += 1
+
+            # Compute the next entry in the lower triangular matrix
+            if row == col:
+                Lij = np.sqrt(A_ij - rowsum)
+
+            else:
+                idx_jj = Lindptr[col+1]-1
+                Ljj = Ldata[idx_jj]
+                Lij = (A_ij - rowsum) / Ljj
+
+            if Lij != 0:
+                Ldata.append(Lij)
+                Lindices.append(col)
+
+        Lindptr.append(len(Lindices))
+
+    return Ldata, Lindices, Lindptr
